190e44cf2342e020ddf2fbd0d8426230394a9d8c
Marcos Nascimento <131052793+mvrcost@users.noreply.github.com>
2024-06-25 09:10:34 -0300

    Adicionando graficos

--- source/analytics.py
+++ source/analytics.py
@@ -148,8 +148,7 @@ sns.palplot( Paleta )
 plt.title('Ministério da Fazenda - paleta de cores', loc='left',fontfamily='serif',fontsize=15, y=1.2)
 #%%
 
-Anl_Emprestimo = Base_Concesao.groupby('Ano').agg(Sum = ('Valor', 'sum'))/10 ** 9
-
+Anl_Emprestimo = Base_Concesao.groupby('Ano').agg(Sum = ('Valor', 'sum')).tail(7)/10 ** 9.
 Anl_Emprestimo.index
 Anl_Emprestimo.sum
 
@@ -157,4 +156,76 @@ Anl_Emprestimo.sum
 #%%
 plt.figure( figsize=(12, 5) )
 plt.title('Gráfico de barra')
-plt.bar( Anl_Emprestimo.index, Anl_Emprestimo.Sum, color=Paleta )
+plt.bar( Anl_Emprestimo.index, Anl_Emprestimo.Sum)
+
+# Tamanho da figura
+fig, ax = plt.subplots(figsize=(12, 5))
+
+# Título e subtítulo
+fig.text(0.09, 1, 'Investindo no Progresso: Evolução Anual dos Pedidos de Empréstimos Municipais/UFs', fontsize=15, fontweight='bold', fontfamily='serif')
+fig.text(0.09, 0.95, 'Análise das Tendências e Necessidades de Financiamento ao Longo dos Anos.', fontsize=12, fontweight='light', fontfamily='serif')
+
+# Gráfico de barras
+bars = ax.bar(
+    Anl_Emprestimo.index, Anl_Emprestimo.Sum,
+    width=0.5, edgecolor='darkgray', linewidth=1.6, color=Paleta[3]
+)
+
+# Remover todas as spines (bordas)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['left'].set_visible(False)
+
+# Linha horizontal no eixo y=0
+ax.axhline( y=0, color='black', linewidth=5.3, alpha=0.7)
+
+# Configurar grid apenas no eixo y
+ax.grid(axis='y', linestyle='-', alpha=0.4)
+ax.grid(axis='x', alpha=0)
+
+# Adicionar rótulo ao eixo y
+ax.set_ylabel('Valores em bilhões (bi)', size=10)
+
+# Configurar o tamanho das marcas dos eixos
+ax.tick_params(axis='both', which='major', labelsize=12)
+
+# Adicionar os valores em cima das barras
+# Iterando sobre cada barra do gráfico
+for bar in bars:
+
+    # Obtendo a altura da barra atual
+    height = bar.get_height()
+
+    # Adicionando uma anotação (valor da barra) no topo de cada barra
+    ax.annotate(
+        # Formato do texto: 'R$' seguido do valor da barra com separadores de milhares e uma casa decimal
+        'R$ {:,.1f}'.format(height),
+        # Posição da anotação: centro da barra
+        xy=(bar.get_x() + bar.get_width() / 2, height),
+        # Deslocamento vertical e horizontal da anotação
+        xytext=(0, 3),
+        # Especificando o sistema de coordenadas do deslocamento da anotação
+        textcoords="offset points",
+        # Alinhamento horizontal e vertical do texto
+        ha='center', va='bottom',
+        # Tamanho da fonte do texto
+        fontsize=10,
+        # Peso da fonte (leve)
+        fontweight='light',
+        # Família da fonte (serif)
+        fontfamily='serif'
+    )
+
+# insiths
+fig.text(
+    0.09, -0.1,
+    '''
+    Considerando a tendência de crescimento observada nos últimos anos,
+    há uma perspectiva de que o volume de pedidos de empréstimo para o ano de 2024 não ultrapasse o registrado em 2018,
+    que foi o maior da série histórica até o momento
+    ''',
+    fontsize=10, fontweight='light', fontfamily='serif'
+)
+
+plt.savefig('Analise-Anual.png', dpi=500, bbox_inches='tight')
